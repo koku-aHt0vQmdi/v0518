fd9b4878744aa3538e70d94ab563069605eb7204
田渕 <tabuti.dat@gmail.com>
2023-05-18 16:11:31 +0900

    保存
    なんか追加した

--- v0518/Form1.Designer.cs
+++ v0518/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            label1 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Yu Gothic UI", 50F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.Location = new Point(100, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 100);
+            button1.TabIndex = 0;
+            button1.Text = "↑";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Yu Gothic UI", 50F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(106, 106);
+            label1.Name = "label1";
+            label1.Size = new Size(84, 89);
+            label1.TabIndex = 1;
+            label1.Text = "+";
+            label1.Click += label1_Click;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Yu Gothic UI", 50F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(200, 100);
+            button2.Name = "button2";
+            button2.Size = new Size(100, 100);
+            button2.TabIndex = 2;
+            button2.Text = "→";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Yu Gothic UI", 50F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(100, 200);
+            button3.Name = "button3";
+            button3.Size = new Size(100, 100);
+            button3.TabIndex = 3;
+            button3.Text = "↓";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Yu Gothic UI", 50F, FontStyle.Regular, GraphicsUnit.Point);
+            button4.Location = new Point(0, 100);
+            button4.Name = "button4";
+            button4.Size = new Size(100, 100);
+            button4.TabIndex = 4;
+            button4.Text = "←";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Label label1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
     }
 }
--- v0518/Form1.cs
+++ v0518/Form1.cs
@@ -6,5 +6,35 @@ namespace v0518
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Å™";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "+";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Å®";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Å´";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Å©";
+        }
     }
 }
